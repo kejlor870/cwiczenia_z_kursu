5823c0c39fc4d2ea8485eb356768c1b1d82a5ce9
kejlor870 <71232096+kejlor870@users.noreply.github.com>
2025-07-19 21:57:14 +0200

    Lista zadan w React+Vite
    - podstawa aplikacji listy zadan (ToDoList)
    - dodawanie zadan z input i wyswietlanie ich w liscie pod formularzem

---
+++ ToDoListViteReact/src/components/List.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+function List(){
+    const [tasks, setTasks] = useState([]);
+    const [input, setInput] = useState('');
+
+    function addTask(e){
+        e.preventDefault();
+
+        if(input.trim() == ""){ // sprawdzanie czy input nie jest pusty / wyciananie wielu spacji
+            return;
+        }
+
+        setTasks([...tasks, input]); // dodawanie nowego zadania do tablicy z poprzednimi zadaniami
+        setInput(""); // wyczyszczenie inputa
+    }
+
+    return(
+        <div id="toDoList">
+            <h1> Lista zadań </h1>
+
+            <form onSubmit={addTask}> {/* wywolanie funkcji przy przeslaniu */}
+                <h3>
+                    Dodaj zadanie: &nbsp;
+                    <input type="text" placeholder="Zadanie..." value={input} onChange={(e) => setInput(e.target.value)}/> 
+                        {/* onchange ustawia wartosc input za pomoca funkcji */}
+                        {/* value jest tu potrzebne po to zeby wyczyscic co jest wpisane w input za pomoca funkcji addTask ( setInput("") ) */}
+                </h3>
+                
+                <button type="submit"> Zapisz </button>
+            </form>
+
+            <ul>
+                {/* Wypisanie z tablicy wartosci */}
+                {tasks.map((task, index) => (
+                    <li key={index}>{task}</li>
+                ))}
+            </ul>
+        </div>
+    );
+
+}
+
+export default List
